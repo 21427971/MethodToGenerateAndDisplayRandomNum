0eda1365cf6a1faf13055ad73dc3f7a667c654b2
Your Name <you@example.com>
2023-10-17 04:26:44 +0200

    Add project files.

---
+++ MethodToGenerateAndDisplayRandomNum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+namespace MethodToGenerateAndDisplayRandomNum
+{ //Msimango KSI
+    class Program
+    {  // A C# console application that allow a user to enter a number.The system will then generate the random numbers and display them on the user
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to generate random numbers");  
+            PrintNumbers();        
+        }
+
+        static void PrintNumbers()
+
+        {                  
+            int num = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            Console.WriteLine("Your Random numbers are : ");
+
+            for (int x=0; x<num; x++)
+            {
+               Console.WriteLine($"{random.Next(1,9)}");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
